e619047b65daf7480a08f04103c6ea629f3421ea
DiptoChakrabarty <diptochuck123@gmail.com>
2020-10-27 14:52:09 +0530

    fix marshmallow errors

--- app/api/schema/activities.py
+++ app/api/schema/activities.py
@@ -1,6 +1,3 @@
-from marshmallow_jsonapi import fields
-from marshmallow_jsonapi.flask import Schema
-
 from app.api.helpers.utilities import dasherize
 from app.models.activity import Activity
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
@@ -10,19 +7,16 @@ class ActivitySchema(SQLAlchemyAutoSchema):
     """
     Api schema for Activity Model
     """
-
     class Meta:
         """
         Meta class for Activity Api Schema
         """
-
         type_ = 'activity'
         self_view = 'v1.activity_detail'
         self_view_kwargs = {'id': '<id>'}
         inflect = dasherize
 
-    model = Activity
-    dump_only = ("id",)
-    load_instance = True
-    include_fk = True
-
+        model = Activity
+        dump_only = ("id",)
+        load_instance = True
+        include_fk = True
